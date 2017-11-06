02a6b452ba4481b01f6dc20892192ff85c1c4866
Student <Student@10.2.5.6>
2017-11-06 16:31:50 +0100

    Do skonczenia

---
+++ src/Zadanie1.java
@@ -0,0 +1,19 @@
+import java.util.LinkedList;
+import java.util.Random;
+import  java.util.Scanner;
+
+	public class Zadanie1 {
+
+
+	public static void main(String[] args) {
+		LinkedList m = new LinkedList();
+		Random generator = new Random();
+		 
+		for(int i=0; i<36; i++) {
+		 
+		   m.add(generator.nextInt());
+		 
+		}
+		
+	}
+	}
