5e65610a4335191a2ff4f7ea73e7fc1d4daf63e5
Student <Student@10.2.5.6>
2017-11-06 16:32:29 +0100

    Do skończenia

---
+++ src/Zadanie2.java
@@ -0,0 +1,20 @@
+
+
+	import java.util.*;
+	import java.lang.*;
+	import java.io.*;
+	 
+	public class Zadanie2 {
+	  public static void main (String[] args) throws java.lang.Exception
+	    {
+	        Random r = new Random();
+	        int[] crr_array = new int[20];
+	 
+	        for(int i = 0; i < 20; i++)
+	        {
+	            crr_array[i] = r.nextInt(10) + 1;
+	        }
+	    }
+
+
+	}
